82cfd0fc61ad0b3de8a4229a3aa65ee4c2f3ba03
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-02-06 06:42:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+def adder(x,y):
+    """Add two numbers together"""
+    return x+y
+
+assert adder(4,6)==9,"expected 4+6 to be 10."
+
+def reverse_str(s):
+    """
+    Returns reverse of input str(s)
+    """
+    return s[::-1]
+#slicing= [start:stop:step] so [::-1]beginning:end:backwards by 1
