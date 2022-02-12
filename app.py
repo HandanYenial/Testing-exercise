6c519bacd895c53d7e01a3a88feab8d8066aff53
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2022-02-12 00:00:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def reverse_str(s):
     Returns reverse of input str(s)
     """
     return s[::-1]
-#slicing= [start:stop:step] so [::-1]beginning:end:backwards by 1
+#slicing= [start:stop:step] so [::-1]beginning:end:backwards by 1.
